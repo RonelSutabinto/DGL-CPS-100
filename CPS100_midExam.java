63eb8bc5c3bb89029e30266d078ea38d2039e4e3
Your Name <you@example.com>
2022-02-14 13:57:43 -0800

    Exam for CPS 100

---
+++ CPS100_midExam.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class CPS100_midExam {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        String string = "Ronel M. Sutabinto";
+        String[] parts = string.split(" ");
+
+
+        //for(int i =0;i<5;i++){
+
+        //}
+
+        System.out.print(parts[1]);
+    }
+}
